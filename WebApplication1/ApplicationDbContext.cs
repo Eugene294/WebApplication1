64127c40144263af05e183bdcc370a7dfc4e8087
Eugene294 <koblia.eo@gmail.com>
2025-03-02 18:22:01 +1000

    Добавьте файлы проекта.

---
+++ WebApplication1/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
